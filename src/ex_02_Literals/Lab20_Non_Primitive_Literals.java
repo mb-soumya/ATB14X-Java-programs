4816aa80315461b27c5796a8b5579ccc664aa78c
mb-soumya <sbadiger774@gmail.com>
2025-09-19 16:42:53 +0530

    ex02_java_basics

---
+++ src/ex_02_Literals/Lab20_Non_Primitive_Literals.java
@@ -0,0 +1,11 @@
+package ex_02_Literals;
+
+public class Lab20_Non_Primitive_Literals
+{
+    public static void main(String[] args) {
+        String name = "soumya";
+//        String is bunch of char
+        int[] arrays_of_items = new int[10];
+
+    }
+}
