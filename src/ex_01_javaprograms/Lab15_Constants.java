2c40cbde9b2f0808ebcefb6663bff34cc912faa6
mb-soumya <sbadiger774@gmail.com>
2025-09-19 15:19:34 +0530

    ex01_java_basics

---
+++ src/ex_01_javaprograms/Lab15_Constants.java
@@ -0,0 +1,12 @@
+package ex_01_javaprograms;
+
+public class Lab15_Constants
+{
+    public static void main(String[] args) {
+        final int a = 40;
+//        a = 41;
+/* since final is constant so will not be changed but again a is assigned with new value
+ so exception*/
+        System.out.println(a);
+        final float PI = 3.14F;
+}}
