b4d8f76ad51932c5e916731efab6b14451c15790
mb-soumya <sbadiger774@gmail.com>
2025-09-19 14:38:12 +0530

    Multiplication

---
+++ src/Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication
+{
+
+        public static void main(String[] args) {
+            int num = 9; // the number to print the table for
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf("%d × %d = %d%n", num, i, num * i);
+            }
+        }
+    }
+
