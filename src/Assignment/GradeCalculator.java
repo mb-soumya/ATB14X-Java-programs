7ffa36f9b1035fb8f86a241d3fea01aa6d9e34ae
mb-soumya <sbadiger774@gmail.com>
2025-10-04 16:52:41 +0530

    GradeCalculator.java

---
+++ src/Assignment/GradeCalculator.java
@@ -0,0 +1,30 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class GradeCalculator
+{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("please enter numbers between 1 to 100");
+         int score =sc.nextInt();
+         if(score >=90 && score <=100)
+         {
+             System.out.println('A');
+         } else if (score >=80 && score <=89) {
+             System.out.println('B');
+         }
+         else if (score >=70 && score <=79) {
+             System.out.println('C');
+         }
+         else if (score >=60 && score <=69) {
+             System.out.println('D');
+         }
+         else if (score >=0 && score <=59) {
+             System.out.println('F');
+         }
+         else {
+             System.out.println("enter valid number");
+         }
+    }
+}
