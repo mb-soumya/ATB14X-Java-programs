03e5feb4d66593b0cfecd1de199366ea2db9fca1
mb-soumya <sbadiger774@gmail.com>
2025-10-05 22:43:07 +0530

    Assignment

---
+++ src/Assignment/Downsider_Rightangletraingle.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class Downsider_Rightangletraingle
+{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n; i>=0; i--)
+        {
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Assignment/Rightangletraingle.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class Rightangletraingle
+{
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
