51a9d6cd883e4e75837e3ae6f7f8f67c6b8acb20
mb-soumya <sbadiger774@gmail.com>
2025-10-19 15:45:37 +0530

    src

---
+++ src/ex_Strings/Lab135_String_Immutable.java
@@ -0,0 +1,17 @@
+package ex_Strings;
+
+public class Lab135_String_Immutable
+{
+        public static void main(String[] args) {
+
+            String name =  "Pramod";
+            name.toUpperCase(); // PRAMOD
+            System.out.println(name); // PRAMOD
+
+
+
+
+
+        }
+    }
+
